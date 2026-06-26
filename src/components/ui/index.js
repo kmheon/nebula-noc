0341e66bedbd3b7b543723ad971533c0ccac7866
MD. Eyaheya Khondoker <kmheon75@gmail.com>
2026-06-26 19:47:06 +0600

    refactor(ui): migrate inventory to Nebula UI kit

---
+++ src/components/ui/index.js
@@ -0,0 +1,24 @@
+/**
+ * ------------------------------------------------------------
+ * Nebula NOC
+ * Shared UI Exports
+ *
+ * Single entry point for the Nebula Design System.
+ * ------------------------------------------------------------
+ */
+
+export { default as Badge } from "./Badge";
+export { default as Button } from "./Button";
+export { default as Card } from "./Card";
+export { default as CardHeader } from "./CardHeader";
+export { default as ConfirmDialog } from "./ConfirmDialog";
+export { default as Drawer } from "./Drawer";
+export { default as EmptyState } from "./EmptyState";
+export { default as LoadingSpinner } from "./LoadingSpinner";
+export { default as Modal } from "./Modal";
+export { default as ProgressBar } from "./ProgressBar";
+export { default as SearchInput } from "./SearchInput";
+export { default as Skeleton } from "./Skeleton";
+export { default as StatCard } from "./StatCard";
+export { default as StatusDot } from "./StatusDot";
+export { default as Tooltip } from "./Tooltip";
